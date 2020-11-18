c671aaf0c34fc4591da5b3475a0bb5682e66fa46
Wira Darma <dewiradarma@gmail.com>
2020-11-18 08:09:18 +0800

    templating site

---
+++ resources/views/site/homepage.blade.php
@@ -0,0 +1,7 @@
+@extends('site.template')
+@section('navbar')
+	@include('site/navbar')
+@endsection
+@section('slider')
+	@include('site/slider')	
+@endsection
---
+++ resources/views/site/metadata.blade.php
@@ -0,0 +1,26 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">    
+<title>Peng Empu | Waterfall</title>
+
+<!-- Favicon -->
+<!-- <link rel="shortcut icon" href="public/site/assets/img/favicon.ico" type="image/x-icon"> -->
+
+<!-- Font awesome -->
+<link href="public/site/assets/css/font-awesome.css" rel="stylesheet">
+<!-- Bootstrap -->
+<link href="public/site/assets/css/bootstrap.css" rel="stylesheet">   
+<!-- Slick slider -->
+<link rel="stylesheet" type="text/css" href="public/site/assets/css/slick.css">          
+<!-- Fancybox slider -->
+<link rel="stylesheet" href="public/site/assets/css/jquery.fancybox.css" type="text/css" media="screen" /> 
+<!-- Theme color -->
+<link id="switcher" href="public/site/assets/css/theme-color/green-theme.css" rel="stylesheet">          
+
+<!-- Main style sheet -->
+<link href="public/site/assets/css/style.css" rel="stylesheet">    
+
+
+<!-- Google Fonts -->
+<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Roboto:400,400italic,300,300italic,500,700' rel='stylesheet' type='text/css'>
